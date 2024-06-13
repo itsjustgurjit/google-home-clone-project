91418f6ff771b18a4a8fc99ed706643ba0fe47cc
Gurjit Singh <gurjitsinghisere@gmail.com>
2024-06-13 22:09:17 +0530

    ui improvement

--- search-engine-clone.js
+++ search-engine-clone.js
@@ -1,12 +1,22 @@
-let inputbox = document.querySelector('input')
+let inputbox = document.querySelector('#input-srh')
 let linkdiv = document.querySelector('.link-div')
+let search_btns = document.querySelectorAll('.search-btn')
 
 let btn = document.querySelector('button')
 
 let baselink = `https://www.google.com/search?q=`
 
-btn.addEventListener('click',(e)=>{
-    e.preventDefault()
+search_btns.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        if(inputbox.value == ''){
+            return
+        }else{
+            dothemagic()
+        }
+    })
+});
+
+function dothemagic(){
     let brokenwords = (inputbox.value.split(' '))
     let modifiedsearch = brokenwords.join('+')
     let freshlinkurl = baselink + modifiedsearch
@@ -17,4 +27,15 @@ btn.addEventListener('click',(e)=>{
     document.body.appendChild(freshlink)
     freshlink.click()
     freshlink.remove()
+}
+
+inputbox.addEventListener('keydown',(event)=>{
+    if(event.key == 'Enter'){
+        if(inputbox.value == ''){
+            return
+        }
+        else{
+            dothemagic()
+        }
+    }
 })
